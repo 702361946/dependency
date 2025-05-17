6f21a5b59a8f0ebcc88260284e06baedc53421d6
702361946 <702361946@qq.com>
2025-05-17 13:41:00 +0800

    music模块
    改造结束

---
+++ modules/_pygame/__init__.py
@@ -0,0 +1,10 @@
+#  Copyright (c) 2025.
+#  702361946@qq.com(https://github.com/702361946)
+
+from ._music import *
+
+log = Log(
+    log_sign="pygame",
+    log_output_to_file_path=f"{log_path}pygame.log",
+    log_output_to_file_mode="w"
+)
