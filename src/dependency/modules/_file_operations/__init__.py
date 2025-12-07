4b92c8d1804ff36b6e3a12c074f34c7ea8ed8723
702361946 <702361946@qq.com>
2025-12-08 00:03:20 +0800

    补全fo init信息

--- src/dependency/modules/_file_operations/__init__.py
+++ src/dependency/modules/_file_operations/__init__.py
@@ -1,6 +1,7 @@
-from ._json import load_json
-from ._file import load_file
+from ._file import File
+from ._json import Json
 
-__all__ = [
-
-]
+__version__ = "0.0.1"
+__author__ = "702361946@qq.com"
+__license__ = "MIT"
+__all__ = ['File', 'Json']
